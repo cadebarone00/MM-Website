1dfe98c4a71d2020dae50d1246e33225dc8dc937
cadebarone00 <cadebarone00@gmail.com>
2026-08-05 21:21:06 -0500

    feat(wagers): add My Portfolio page

---
+++ app/wagers/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import { MyWagersList } from "@/components/wagers/MyWagersList";
+
+export default function PortfolioPage() {
+  return (
+    <div className="px-4 pt-5 sm:px-7">
+      <MyWagersList />
+    </div>
+  );
+}
