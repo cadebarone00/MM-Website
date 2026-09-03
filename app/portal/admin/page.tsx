75db00e1ceddce3ac812d78608b92f406593a12b
cadebarone00 <cadebarone00@gmail.com>
2026-09-02 23:02:43 -0500

    settings

--- app/portal/admin/page.tsx
+++ app/portal/admin/page.tsx
@@ -51,6 +51,12 @@ export default async function TigerCenterPage() {
           >
             Wager Types
           </Link>
+          <Link
+            href="/portal/admin/broadcast-controls"
+            className="rounded-lg border-2 border-maroon-700 bg-maroon-700 px-6 py-8 text-center font-serif text-xl font-bold text-white transition hover:bg-maroon-800"
+          >
+            Broadcast Controls
+          </Link>
         </div>
       </div>
       <Link
